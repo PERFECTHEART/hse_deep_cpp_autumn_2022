5c26a5f1787f8af333b90ba17fffe6a94102fe07
PERFECTHEART <101324009+PERFECTHEART@users.noreply.github.com>
2022-12-14 22:09:35 +0300

    Add files via upload

---
+++ matrix.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <iostream>
+#include <cassert>
+#include <stdexcept>
+
+using namespace std;
+
+#define BASE_TYPE	int
+
+class Matrix
+{
+public:
+	class Proxy {
+	public:
+		Proxy(BASE_TYPE* pRow) : row(pRow) {}
+		~Proxy() {}
+		BASE_TYPE & operator[] (const size_t & index)const;
+	private:
+		BASE_TYPE* row;
+	};
+
+private:
+  	BASE_TYPE **matrix;
+  	size_t	d1;
+  	size_t	d2;
+public:
+	Matrix (const size_t &d1,const size_t &d2);
+	Matrix(const Matrix &rhs);
+	~Matrix();
+	Matrix	operator =(const Matrix &rhs)const;
+	Matrix  operator +(const Matrix &rhs)const ;
+	Matrix  operator *=(const size_t &num)const;
+	bool	operator ==(const Matrix &rhs)const;
+	size_t 	getRows()const;
+	size_t 	getColumns()const;
+	Proxy operator[](const size_t &index )const;
+	friend ostream & operator<<( ostream & , const Matrix & );
+};
